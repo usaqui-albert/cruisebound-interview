6ce50f20e9e6670c70c3ccdee943d96f5a2614ac
Albert Usaqui <usaqui.albert@gmail.com>
2025-02-11 15:50:45 -0400

    feat: add react query and set up its provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { QueryProvider } from "@/components/providers";
 import "./globals.css";
 
 const inter = Inter({
@@ -19,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} antialiased`}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
